6544ab237102a8db63186bdb5cd2721b8dd3c7f7
Chang <gksckddn_@naver.com>
2020-11-30 21:30:32 -0500

    ..

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Transform player;
+    Transform tr;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Vector3 doorToPlayer = player.position - tr.position;
+        float dotProduct = Vector3.Dot(transform.up, doorToPlayer);
+        Debug.Log(dotProduct);
+    }
+}
