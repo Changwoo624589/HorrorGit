c683f83f8b924fa5e4e5ddb9247c21421f0ad310
ava <kersa689@newschool.edu>
2020-12-05 23:05:47 -0500

    duplicated caitlin's scene added lookAtPlayer script

---
+++ Assets/Scripts/lookAtPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtPlayer : MonoBehaviour
+{
+ public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+         Vector3 targetPostition = new Vector3( target.position.x, 
+                                        this.transform.position.y, 
+                                        target.position.z ) ;
+        this.transform.LookAt( targetPostition ) ;
+    }
+}
