7d11408307738a112817ccc1d71378580c30843c
Chang <gksckddn_@naver.com>
2020-12-16 00:38:59 -0500

    Auto stash before merge of "dev" and "origin/dev"

---
+++ Assets/Scripts/Credit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    public GameObject creditPanel;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (GameManager.Instance.credit)
+        {
+            creditPanel.SetActive(true);
+        }
+        else { creditPanel.SetActive(false); }
+    }
+}
