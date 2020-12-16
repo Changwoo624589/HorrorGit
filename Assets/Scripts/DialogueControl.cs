0ed56e85b3048093da91a81a133b90d102b54fa9
Caitlin Keating <keatc512@newschool.edu>
2020-12-16 05:21:45 -0800

    fixing merge conflict, adding dialogue script

--- Assets/Scripts/DialogueControl.cs
+++ Assets/Scripts/DialogueControl.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueControl : MonoBehaviour
 {
+    public Queue<string> sentences;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue(Dialogue dialogue)
     {
-        
+        Debug.Log("Starting conversation with" + dialogue.name);
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence); 
+        }
+        DisplayNextSentence(); 
+
     }
+    public void DisplayNextSentence()
+    {
+        //
+    }
+   
 }
---
+++ Assets/TriggerDialogue.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class TriggerDialogue : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "CanTalkWith")
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+               
+                StartCoroutine(OpenDialogue()); 
+            }
+        }
+    }
+
+    IEnumerator OpenDialogue()
+    {
+
+        WaitForSeconds waitTime = new WaitForSeconds(1); 
+        yield return waitTime;
+        dialoguePanel.SetActive(true);
+        
+
+        WaitForSeconds waitTime2 = new WaitForSeconds(5);
+       yield return waitTime2;
+        dialoguePanel.SetActive(false);
+        
+
+    }
+
+}
+
+
+    
